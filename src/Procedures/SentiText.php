1fa7cffca436a5cb6a81ebaf6d9a94fba62c78e4
Peter Lounton <plounton@me.com>
2024-03-08 12:41:52 +0000

    refactor: update SentiText class with type declarations
    This commit introduces several improvements to the SentiText class,
    including the addition of type declarations for better code clarity
    and reliability. Notably, properties and methods now have explicit
    types, enhancing the predictability of the code's behaviour. The
    constructor visibility has been changed to public, aligning with
    common practices for object instantiation. Furthermore, the
    implementation of PUNC_LIST as a class constant and the modification
    of method visibilities from private to public where necessary, improve
    the class's usability. Code redundancy has been reduced through the
    removal of duplicated or unused methods, contributing to a cleaner
    and more maintainable codebase.

--- src/Procedures/SentiText.php
+++ src/Procedures/SentiText.php
@@ -8,22 +8,18 @@ namespace Sentiment\Procedures;
 
 class SentiText
 {
+    public const array PUNC_LIST                 = [".", "!", "?", ",", ";", ":", "-", "'", "\"", "!!", "!!!", "??", "???", "?!?", "!?!", "?!?!", "!?!?"];
+    public array|false|null $words_and_emoticons = null;
+    public bool|null $is_cap_diff                = null;
+    private string $text                         = "";
 
-    private $text = "";
-    public $words_and_emoticons = null;
-    public $is_cap_diff = null;
-
-    const PUNC_LIST = [".", "!", "?", ",", ";", ":", "-", "'", "\"",
-             "!!", "!!!", "??", "???", "?!?", "!?!", "?!?!", "!?!?"];
-
-
-    function __construct($text)
+    public function __construct($text)
     {
         //checking that is string
         //if (!isinstance(text, str)){
         //    text = str(text.encode('utf-8'));
         //}
-        $this->text = $text;
+        $this->text                = $text;
         $this->words_and_emoticons = $this->_words_and_emoticons();
         // doesn't separate words from\
         // adjacent punctuation (keeps emoticons & contractions)
@@ -33,74 +29,22 @@ class SentiText
     /*
         Remove all punctation from a string
     */
-    function strip_punctuation($string)
-    {
-        //$string = strtolower($string);
-        return preg_replace("/[[:punct:]]+/", "", $string);
-    }
 
-    function array_count_values_of($haystack, $needle)
-    {
-        if (!in_array($needle, $haystack, true)) {
-            return 0;
-        }
-        $counts = array_count_values($haystack);
-        return $counts[$needle];
-    }
-
-    /*
-        Check whether just some words in the input are ALL CAPS
-
-        :param list words: The words to inspect
-        :returns: `True` if some but not all items in `words` are ALL CAPS
-    */
-    private function allcap_differential($words)
-    {
-
-        $is_different = false;
-        $allcap_words = 0;
-        foreach ($words as $word) {
-            //ctype is affected by the local of the processor see manual for more details
-            if (ctype_upper($word)) {
-                $allcap_words += 1;
-            }
-        }
-        $cap_differential = count($words) - $allcap_words;
-        if ($cap_differential > 0 && $cap_differential < count($words)) {
-            $is_different = true;
-        }
-        return $is_different;
-    }
-
-    function _words_only()
-    {
-        $text_mod = $this->strip_punctuation($this->text);
-        // removes punctuation (but loses emoticons & contractions)
-        $words_only = preg_split('/\s+/', $text_mod);
-        # get rid of empty items or single letter "words" like 'a' and 'I'
-        $works_only = array_filter($words_only, function ($word) {
-            return strlen($word) > 1;
-        });
-        return $words_only;
-    }
-
-    function _words_and_emoticons()
+    public function _words_and_emoticons(): array|false
     {
 
         $wes = preg_split('/\s+/', $this->text);
 
         # get rid of residual empty items or single letter words
-        $wes = array_filter($wes, function ($word) {
-            return strlen($word) > 1;
-        });
+        $wes = array_filter($wes, static fn($word) => strlen((string) $word) > 1);
         //Need to remap the indexes of the array
-        $wes = array_values($wes);
+        $wes        = array_values($wes);
         $words_only = $this->_words_only();
 
         foreach ($words_only as $word) {
             foreach (self::PUNC_LIST as $punct) {
                 //replace all punct + word combinations with word
-                $pword = $punct .$word;
+                $pword = $punct . $word;
 
 
                 $x1 = $this->array_count_values_of($wes, $pword);
@@ -112,7 +56,7 @@ class SentiText
                 }
                 //Do the same as above but word then punct
                 $wordp = $word . $punct;
-                $x2 = $this->array_count_values_of($wes, $wordp);
+                $x2    = $this->array_count_values_of($wes, $wordp);
                 while ($x2 > 0) {
                     $i = array_search($wordp, $wes, true);
                     unset($wes[$i]);
@@ -123,5 +67,58 @@ class SentiText
         }
 
         return $wes;
+    }
+
+    public function _words_only(): array|false
+    {
+        $text_mod = $this->strip_punctuation($this->text);
+        // removes punctuation (but loses emoticons & contractions)
+        $words_only = preg_split('/\s+/', $text_mod);
+        # get rid of empty items or single letter "words" like 'a' and 'I'
+        $works_only = array_filter($words_only, fn($word) => strlen((string) $word) > 1);
+
+        return $words_only;
+    }
+
+    /*
+        Check whether just some words in the input are ALL CAPS
+
+        :param list words: The words to inspect
+        :returns: `True` if some but not all items in `words` are ALL CAPS
+    */
+
+    public function strip_punctuation($string): array|string|null
+    {
+        //$string = strtolower($string);
+        return preg_replace("/[[:punct:]]+/", "", (string) $string);
+    }
+
+    public function array_count_values_of($haystack, $needle)
+    {
+        if (! in_array($needle, $haystack, true)) {
+            return 0;
+        }
+        $counts = array_count_values($haystack);
+
+        return $counts[$needle];
+    }
+
+    private function allcap_differential($words): bool
+    {
+
+        $is_different = false;
+        $allcap_words = 0;
+        foreach ($words as $word) {
+            //ctype is affected by the local of the processor see manual for more details
+            if (ctype_upper((string) $word)) {
+                $allcap_words += 1;
+            }
+        }
+        $cap_differential = count($words) - $allcap_words;
+        if ($cap_differential > 0 && $cap_differential < count($words)) {
+            $is_different = true;
+        }
+
+        return $is_different;
     }
 }
